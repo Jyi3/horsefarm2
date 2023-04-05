0f4576354d8aa4f6bd9e2d04c5c482089691f9d8
Ethan Thompson <ethan.m.thompson@gmail.com>
2023-04-04 23:21:02 -0400

    Had to remake notedb.php and Note.php (see notedb2.php and Note2.php) due to mismatch in regards to SQL-side schema.

---
+++ domain/Note2.php
@@ -0,0 +1,39 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class Note {
+
+    private $horseID;
+    private $noteDate;
+    private $noteTimestamp;
+    private $note;
+    private $username;
+    
+    function __construct($horseID,$noteDate,$noteTimestamp,$note,$username) {
+        $this->horseID = $horseID;
+        $this->noteDate = $noteDate;
+        $this->noteTimestamp = $noteTimestamp;
+        $this->note = $note;
+        $this->username = $username;
+    }
+    function get_horseID() {
+        return $this->horseID;
+    }
+    function get_noteDate() {
+        return $this->noteDate;
+    }
+    function get_timestamp() {
+        return $this->noteTimestamp;
+    }
+    function get_note() {
+        return $this->note;
+    }
+    function get_username() {
+        return $this->username;
+    }
+}
