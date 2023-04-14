5a8b7e56bcb09d08f4349b7974a7e4325807e891
Nicolas Guzzone <125775407+nguzz1@users.noreply.github.com>
2023-04-14 01:57:30 -0400

    created autoAssignBehaviors function

--- addBehavior.php
+++ addBehavior.php
@@ -8,6 +8,106 @@
     include('database/person.php');
 
 //Will add a function here that takes in a horseID, and depending on their rank automatically add the right behaviors of the same color rank and below
+/*
+Function name: autoAssignBehaviors
+Description: Automatically assigns all behaviors of certain levels, depending on the colorRank of the horse.
+             So if the horse is Red, it automatically gets all green, yellow and red behaviors. 
+             If it is yellow it automatically gets all green and yellow behaviors.
+             If it is green it only gets all green behaviors assigned to it. 
+Parameters: 
+            HorseID --> ID of the horse who we want to add a behavior to    (type: string)
+
+Return Value(s):
+            True --> Returns true if the behavior was succesfully added to the given horse
+            False --> Returns false if the system was unable to assign the behavior to the given horse
+*/
+function autoAssignBehaviors($horseID){
+    //Horse object that corresponds to the ID inputted
+    $horse = retrieve_horse($horseID);
+    //Color of the inputted horse (defines what behaviors get assigned to it)
+    $color = $horse->get_colorRank();
+    //If the horse is has a green colorRank, then it gets all the green behaviors
+    if($color == "GREEN"){
+
+        $behaviors = get_all_green_behaviors();
+        //Gets the all the titles of all green behaviors (get it in a seperate array so we can call it by its index in the for loop below)
+        $behaviorTitles = array_keys($behaviors);
+        //number of behaviors to be assigned
+        $numBehaves = count($behaviors);
+        $con = connect();
+
+        //Loops through all the behaviors that need to be assigned to the inputted horse
+        for($i=0; $i < $numBehaves; $i++){
+            
+            //Title of the current behavior that the loop is on
+            $behaveName = $behaviorTitles[$i];
+            //Query that gets all the info of the current behavior so we can then make a behavior object out of it to then be asigned
+            $query = "SELECT * FROM behaviorDB WHERE title='". $behaveName . "';";
+            $result= mysqli($con, $query);
+            $curBehavior = make_a_behavior($result);
+            //assigns the current behavior to the given horse
+            assignBehavior($horseID,$curBehavior);
+            return TRUE;
+        }
+
+    }
+    elseif($color == "YELLOW"){
+        $greens = get_all_green_behaviors();
+        $yellows = get_all_yellow_behaviors();
+        //One big array of all green and yellow behaviors
+        $behaviors = array_merge($greens,$yellows);
+        //Gets all the titles of all yellow and green behaviors and puts it into a seperate array (so we can call the titles by its index in the for loop below)
+        $behaviorTitles = array_keys($behaviors);
+        //number of behaviors to be assigned
+        $numBehaves = count($behaviors);
+        $con = connect()
+
+        //Loops through all the behaviors needed to be assigned
+        for($i=0; $i < $numBehaves; $i++){
+            //Title of the current behavior the loop is on
+            $behaveName = $behaviorTitles[$i];
+            //Query that gets all info of the current behavior to then make a behavior object out of it
+            $query = "SELECT * FROM behaviorDB WHERE title='". $behaveName . "';";
+            $result = mysqli($con,$query);
+            $curBehavior = make_a_behavior($result);
+            //assigns current behavior to the given horse
+            assignBehavior($horseID, $curBehavior);
+        }
+        return TRUE;
+    }
+    elseif($color == "RED"){
+
+        $greens = get_all_green_behaviors();
+        $yellows = get_all_yellow_behaviors();
+        $reds = get_all_red_behaviors();
+        //One big array of all green, yellow, and red behaviors
+        $behaviors = array_merge($greens,$yellows,$reds);
+        //Gets all the titles of all red, yellow and green behaviors and puts it into a seperate array (so we can call the titles by its index in the for loop below)
+        $behaviorTitles = array_keys($behaviors);
+        //number of behaviors to be assigned
+        $numBehaves = count($behaviors);
+        $con = connect()
+
+        //Loops through all the behaviors needed to be assigned
+        for($i=0; $i < $numBehaves; $i++){
+            //Title of the current behavior the loop is on
+            $behaveName = $behaviorTitles[$i];
+            //Query that gets all info of the current behavior to then make a behavior object out of it
+            $query = "SELECT * FROM behaviorDB WHERE title='". $behaveName . "';";
+            $result = mysqli($con,$query);
+            $curBehavior = make_a_behavior($result);
+            //assigns current behavior to the given horse
+            assignBehavior($horseID, $curBehavior);
+        }
+        return TRUE;
+    }
+    else{
+        return FALSE;
+    }
+}
+
+
+
 
 /*
 Function name: assignBehavior
